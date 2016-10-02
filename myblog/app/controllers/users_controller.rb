f2773aa4dd4d15bc3b444812feb2fda054050201
Phan Tân <phanvantan95@gmail.com>
2016-10-02 21:05:20 +0700

    show + edit

--- myblog/app/controllers/users_controller.rb
+++ myblog/app/controllers/users_controller.rb
@@ -13,9 +13,19 @@ class UsersController < ApplicationController
   	redirect_to users_url
   end
 
-  def show
-  	@user = User.find(params[:id])
+   def show
+    @user = User.find(params[:id])
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to user_path(params[:id])
   end
 
 
